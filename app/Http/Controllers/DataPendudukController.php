c269f72622418d2c3aa5178e7b59653341238fe2
Dela Farahita Zain <zainfarahitadela@gmail.com>
2024-04-16 10:47:48 +0700

    CRUD Data Penduduk

--- app/Http/Controllers/DataPendudukController.php
+++ app/Http/Controllers/DataPendudukController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\DataPendudukModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Yajra\DataTables\DataTables;
 
 class DataPendudukController extends Controller
 {
-    //
+    public function index()
+    {
+        $breadcrumb = (object) [
+            'title' => 'Data Penduduk',
+            'list' => ['Home', 'Data Penduduk']
+        ];
+
+        $page = (object) [
+            'title' => 'Data Penduduk yang terdaftar dalam sistem'
+        ];
+
+        $activeMenu = 'Data Penduduk'; // set menu yang sedang aktif
+        $dataPenduduk = DataPendudukModel::all(); // ambil data level untuk filter level
+
+        return view('admin.data_penduduk.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'Data Penduduk' => $dataPenduduk, 'activeMenu' => $activeMenu]);
+    }
 }
