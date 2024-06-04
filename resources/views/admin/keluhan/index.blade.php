9447a524d463914d98ce3d5853047f030b24db24
halurabiyyu <halurabiyyu@gmail.com>
2024-06-04 18:41:48 +0700

    fix: fixing image keluhan if null

--- resources/views/admin/keluhan/index.blade.php
+++ resources/views/admin/keluhan/index.blade.php
@@ -13,7 +13,9 @@
                             <h6 class="card-subtitle mb-2 text-muted">Asal RT: {{ $keluhan->rt }}</h6>
                             <p class="card-text">{{ $keluhan->keluhan }}</p>
                             <P class="card-text">{{ $keluhan->created_at }}</P>
-                            <img style="width: 200px; height:200px" src="{{asset('storage/img_keluhan/' . $keluhan->foto)}}" alt="{{$keluhan->keluhan}}">
+                            @if ($keluhan->foto)
+                                <img style="width: 200px; height:200px" src="{{asset('storage/img_keluhan/' . $keluhan->foto)}}" alt="{{$keluhan->keluhan}}">
+                            @endif
                             <!-- <a href="{{ route('keluhan.show', $keluhan->id) }}" class="card-link">Lihat Detail</a> -->
                         </div>
                     </div>
